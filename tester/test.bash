cb1886829a35d1f6aaef542da1ae6155c76e5071
Marco Miglierina <marco.miglierina@icloud.com>
2015-11-03 15:44:56 +0100

    Add first test

---
+++ tester/test.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+DEBUG="off"
+MAXTRIPLES=200000
+
+RESULTSFILE=/tmp/`date +%Y%m%d-%H%M%S`.txt
+if [[ $DEBUG ]] ; then
+  RESULTSFILE=/tmp/debug.txt
+  rm $RESULTSFILE
+fi
+
+echo "Waiting for data analyzer to be ready"
+until $(curl --output /dev/null --silent --head --fail http://data-analyzer:8175/queries); do
+    printf '.'
+    sleep 5
+done
+
+curl -sS --fail --data-urlencode "action=update" --data-urlencode "queryBody=clear all" data-analyzer:8175/kb > /dev/null
+i="0"
+while [ $i -lt $MAXTRIPLES ]
+do
+  ts=$(date +%s%N)
+  curl -G -sS --fail --data-urlencode 'query=
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX ex: <http://example.org#>
+    SELECT (COUNT (*) as ?count)
+    WHERE
+    {
+      ?s rdf:type ex:Person ;
+        ex:sonOf ?p .
+      ?p rdf:type ex:Person .
+    }
+    ' data-analyzer:8175/kb > /dev/null
+  tt=$((($(date +%s%N) - $ts)/1000000))
+  echo $[5*$i]","$tt >> $RESULTSFILE
+  i=$[$i+1]
+  curl -sS --fail --data-urlencode "action=update" --data-urlencode 'queryBody=
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  PREFIX ex: <http://example.org#>
+  INSERT DATA
+  {
+    <http://person#'${i}'> rdf:type ex:Person .
+    <http://person#'${i}'> ex:sonOf <http://person#'$[$i+1]'> .
+    <http://person#'${i}'> ex:sonOf <http://person#'$[$i+2]'> .
+    <http://person#'${i}'> ex:sonOf <http://person#'$[$i+3]'> .
+    <http://person#'${i}'> ex:sonOf <http://person#'$[$i+4]'> .
+  }' data-analyzer:8175/kb > /dev/null
+done
